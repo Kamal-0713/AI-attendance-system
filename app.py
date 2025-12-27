117b9987ce664d0f29c986760a924464a69733a8
Kamal-0713 <kamaleshmr2006@gmail.com>
2025-12-27 23:10:07 +0530

    Removed dataset and attendance from repo

--- app.py
+++ app.py
@@ -1,43 +1,25 @@
 from flask import Flask, render_template, request
-import cv2
-import os
+import os, base64
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def index():
-    if request.method == 'POST':
-        name = request.form['username']
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-        path = os.path.join('dataset', name)
-        os.makedirs(path, exist_ok=True)
+@app.route("/upload", methods=["POST"])
+def upload():
+    data = request.get_json()
+    name = data["username"]
+    img = data["img"].split(",")[1]
 
-        cam = cv2.VideoCapture(0)
-        face_cascade = cv2.CascadeClassifier(
-            cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-        )
+    folder = f"dataset/{name}"
+    os.makedirs(folder, exist_ok=True)
 
-        count = 0
-        while count < 30:
-            ret, frame = cam.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray,1.3,5)
+    img_count = len(os.listdir(folder)) + 1
+    with open(f"{folder}/{img_count}.jpg", "wb") as f:
+        f.write(base64.b64decode(img))
 
-            for (x,y,w,h) in faces:
-                count += 1
-                face = gray[y:y+h, x:x+w]
-                face = cv2.resize(face,(128,128))
-                cv2.imwrite(f"{path}/{count}.jpg", face)
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    return {"status":"saved"}
 
-            cv2.imshow("Capturing Dataset", frame)
-            if cv2.waitKey(1) == 27:
-                break
-
-        cam.release()
-        cv2.destroyAllWindows()
-        return f"Dataset created for {name}"
-
-    return render_template('index.html')
-
-app.run(debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
